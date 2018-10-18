d3ffb30f8b7bf58b61f7cf4e9f2a68a52530daae
Denis Kanivets <denis.kanivets@ukr.net>
2018-10-18 18:48:32 +0300

    all done(trial)

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,18 @@ function hide(){
     bar.style.display = 'block';
     times.style.display = 'none';
 }
+
+
+//slick-slider carousel, jQuery plugin
+
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        focusOnSelect: true,
+        arrows: true,
+        prevArrow: $('.fa-chevron-left'),
+        nextArrow: $('.fa-chevron-right'),
+    })
+});
